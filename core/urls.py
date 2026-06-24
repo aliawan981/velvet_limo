3237780f1232e9ca3f9c690ba8c0e31b310a864d
aliawan981 <115535619+aliawan981@users.noreply.github.com>
2026-06-24 16:12:37 +0500

    velvet limo project commit

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from pages import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('fleet/', views.fleet, name='fleet'),
+    path('services/', views.services, name='services'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+]
---
+++ pages/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def fleet(request):
+    return render(request, 'fleet-list.html')
+
+def services(request):
+    return render(request, 'service-grid.html')
+
+def blog(request):
+    return render(request, 'blog-list.html')
+
+def contact(request):
+    return render(request, 'contact.html')
